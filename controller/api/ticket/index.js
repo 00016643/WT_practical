2f8c579f463670ebc7f341f92d17a0448fa824c2
00016643 <00016643wiut@gmail.com>
2024-02-19 12:33:32 +0500

    creating APIs

---
+++ controller/api/ticket/index.js
@@ -0,0 +1,17 @@
+// import specific service class
+const ticket_service = require('../../../service/ticket')
+
+// mention the service's needed actions (methods)
+const ticket_controller = {
+    getAll(req, res) {
+        res.json(ticket_service.getAll())
+    },
+    create(req, res) {
+        res.status(201).json(
+            ticket_service.create(req, res)
+        )
+    }
+
+}
+
+module.exports = ticket_controller
